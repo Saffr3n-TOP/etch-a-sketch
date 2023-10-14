0886d0e0816e6770fe21e9ef015b4ce3acec6472
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-14 22:32:07 +0300

    Add mouseover event for cell to change its color
    Remove unused const cellsAmount from populateGrid function

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const gridWidth = 16;
 const gridHeight = 16;
 
 function populateGrid() {
-  const cellsAmount = gridWidth * gridHeight;
-
   for (let i = 0; i < gridHeight; i++) {
     for (let j = 0; j < gridWidth; j++) {
       const cell = document.createElement('div');
       cell.className = 'cell';
+
+      cell.addEventListener('mouseover', () => {
+        cell.style.background = 'black';
+      }, { once: true });
+
       if (j < gridWidth - 1) cell.style.borderRight = '1px solid black';
       if (i < gridHeight - 1) cell.style.borderBottom = '1px solid black';
+
       grid.appendChild(cell);
     }
   }
