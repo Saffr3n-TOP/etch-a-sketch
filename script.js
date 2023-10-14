6c79a7382c3393750e35ca629ff8de3f0b818755
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-15 01:30:19 +0300

    Randomize and darken color on mouse over cell

--- script.js
+++ script.js
@@ -22,21 +22,42 @@ changeGridWidthBtn.addEventListener('click', () => {
 
 function populateGrid() {
   grid.innerHTML = '';
+  grid.style.gridTemplateColumns = `repeat(${gridWidth}, auto)`;
 
-  cellSize = grid.offsetHeight / gridWidth;
+  const cellSize = grid.offsetHeight / gridWidth;
 
   for (let i = 0; i < gridWidth; i++) {
     for (let j = 0; j < gridWidth; j++) {
       const cell = document.createElement('div');
       cell.className = 'cell';
+      cell.setAttribute('data-intensity', 0);
       cell.style.width = cellSize + 'px';
       cell.style.height = cellSize + 'px';
 
-      cell.addEventListener(
-        'mouseover',
-        () => (cell.style.background = 'black'),
-        { once: true }
-      );
+      cell.addEventListener('mouseover', () => {
+        const intensity = +cell.getAttribute('data-intensity') + 1;
+
+        if (intensity > 10) return;
+
+        const minRgbChannelValue = Math.round(256 - (256 / 9) * intensity);
+        const maxRgbChannelValue = Math.round(256 - (256 / 9) * (intensity - 1));
+
+        const redAmount = getRandomRgbChannelValue(
+          minRgbChannelValue,
+          maxRgbChannelValue
+        );
+        const greenAmount = getRandomRgbChannelValue(
+          minRgbChannelValue,
+          maxRgbChannelValue
+        );
+        const blueAmount = getRandomRgbChannelValue(
+          minRgbChannelValue,
+          maxRgbChannelValue
+        );
+
+        cell.setAttribute('data-intensity', intensity);
+        cell.style.background = `rgb(${redAmount}, ${greenAmount}, ${blueAmount}`;
+      });
 
       if (j < gridWidth - 1) cell.style.borderRight = '1px solid black';
       if (i < gridWidth - 1) cell.style.borderBottom = '1px solid black';
@@ -44,8 +65,10 @@ function populateGrid() {
       grid.appendChild(cell);
     }
   }
+}
 
-  grid.style.gridTemplateColumns = `repeat(${gridWidth}, auto)`;
+function getRandomRgbChannelValue(minValue, maxValue) {
+  return Math.floor(Math.random() * (maxValue - minValue) + minValue);
 }
 
 populateGrid();
